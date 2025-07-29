784b1d475ead7ef9bf82f2808a035421e85bb035
ibrarzahoorr <ibrarzahoor0@gmail.com>
2025-07-29 11:52:49 +0500

    fleetupdate

--- Video 2/const.js
+++ Video 2/const.js
@@ -1,3 +1,5 @@
 const c = 10;
 console.log("c is declared using const:", c);
 console.log("This is a variable declaration in JavaScript using const");
+const name = "John";
+console.log("Name is declared using const:", name);
